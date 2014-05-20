67559a26126914bdd1dc292bb9b68bf90c6695de
Igor Fedorenko <igor@ifedorenko.com>
2014-05-19 22:35:37 -0400

    introduced mojo execution configurator
    Signed-off-by: Igor Fedorenko <igor@ifedorenko.com>

---
+++ io.takari.m2e.incrementalbuild.core/src/io/takari/m2e/incrementalbuild/core/internal/MojoExecutionProjectConfigurator.java
@@ -0,0 +1,30 @@
+package io.takari.m2e.incrementalbuild.core.internal;
+
+import org.apache.maven.plugin.MojoExecution;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.m2e.core.lifecyclemapping.model.IPluginExecutionMetadata;
+import org.eclipse.m2e.core.project.IMavenProjectFacade;
+import org.eclipse.m2e.core.project.configurator.AbstractBuildParticipant;
+import org.eclipse.m2e.core.project.configurator.AbstractProjectConfigurator;
+import org.eclipse.m2e.core.project.configurator.MojoExecutionBuildParticipant;
+import org.eclipse.m2e.core.project.configurator.ProjectConfigurationRequest;
+
+/**
+ * The purpose of this configurator is to (hopefully) trigger discovery of
+ * takari.m2e.incrementalbuild feature during m2e project import and configuration.
+ */
+public class MojoExecutionProjectConfigurator extends AbstractProjectConfigurator {
+
+  @Override
+  public void configure(ProjectConfigurationRequest request, IProgressMonitor monitor)
+      throws CoreException {
+    // nothing to configure
+  }
+
+  @Override
+  public AbstractBuildParticipant getBuildParticipant(IMavenProjectFacade projectFacade,
+      MojoExecution execution, IPluginExecutionMetadata executionMetadata) {
+    return new MojoExecutionBuildParticipant(execution, true);
+  }
+}
