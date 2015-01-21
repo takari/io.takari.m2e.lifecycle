cb8c57da253d289c7fff1e3455002bde3b11f6bc
Igor Fedorenko <igor@ifedorenko.com>
2015-01-21 10:57:54 -0500

    generated maven pom.properties during workspace build
    Signed-off-by: Igor Fedorenko <igor@ifedorenko.com>

---
+++ io.takari.m2e.jar/src/io/takari/m2e/jar/internal/JarProjectConfigurator.java
@@ -0,0 +1,44 @@
+package io.takari.m2e.jar.internal;
+
+import org.apache.maven.plugin.MojoExecution;
+import org.apache.maven.plugin.descriptor.MojoDescriptor;
+import org.apache.maven.plugin.descriptor.PluginDescriptor;
+import org.codehaus.plexus.util.xml.Xpp3Dom;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.m2e.core.lifecyclemapping.model.IPluginExecutionMetadata;
+import org.eclipse.m2e.core.project.IMavenProjectFacade;
+import org.eclipse.m2e.core.project.configurator.AbstractBuildParticipant;
+import org.eclipse.m2e.core.project.configurator.AbstractProjectConfigurator;
+import org.eclipse.m2e.core.project.configurator.MojoExecutionBuildParticipant;
+import org.eclipse.m2e.core.project.configurator.ProjectConfigurationRequest;
+
+public class JarProjectConfigurator extends AbstractProjectConfigurator {
+
+  @Override
+  public void configure(ProjectConfigurationRequest request, IProgressMonitor monitor)
+      throws CoreException {}
+
+  @Override
+  public AbstractBuildParticipant getBuildParticipant(IMavenProjectFacade projectFacade,
+      MojoExecution execution, IPluginExecutionMetadata executionMetadata) {
+    Xpp3Dom configuration = execution.getConfiguration();
+
+    if (Boolean.parseBoolean(configuration.getChild("skip").getValue())) {
+      return null;
+    }
+
+    PluginDescriptor pluginDescriptor = execution.getMojoDescriptor().getPluginDescriptor();
+    MojoDescriptor descriptor = pluginDescriptor.getMojo("pom-properties");
+
+    String _executionId = "m2e-takari-lifecycle:" + execution.getExecutionId() + ":pom-properties";
+    MojoExecution _execution =
+        new MojoExecution(execution.getPlugin(), "pom-properties", _executionId);
+    _execution.setConfiguration(execution.getConfiguration());
+    _execution.setMojoDescriptor(descriptor);
+    _execution.setLifecyclePhase(execution.getLifecyclePhase());
+
+    return new MojoExecutionBuildParticipant(_execution, true);
+  }
+
+}
---
+++ io.takari.m2e.lifecycle.test/src/io/takari/m2e/lifecycle/test/PomPropertiesTest.java
@@ -0,0 +1,82 @@
+package io.takari.m2e.lifecycle.test;
+
+import io.takari.m2e.test.WorkspaceChangeRecorder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IncrementalProjectBuilder;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.m2e.tests.common.AbstractMavenProjectTestCase;
+
+@SuppressWarnings("restriction")
+public class PomPropertiesTest extends AbstractMavenProjectTestCase {
+  private final WorkspaceChangeRecorder recorder = new WorkspaceChangeRecorder("target/classes");
+
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+    ResourcesPlugin.getWorkspace().addResourceChangeListener(recorder);
+  }
+
+  @Override
+  protected void tearDown() throws Exception {
+    ResourcesPlugin.getWorkspace().removeResourceChangeListener(recorder);
+    super.tearDown();
+  }
+
+  public void testPomProperties() throws Exception {
+    IProject project = importProject("projects/pomproperties/pom.xml");
+    waitForJobsToComplete();
+    project.build(IncrementalProjectBuilder.FULL_BUILD, monitor);
+    assertNoErrors(project);
+
+    String path =
+        "target/classes/META-INF/maven/io.takari.m2e.incrementalbuild.test/pomproperties/pom.properties";
+    IFile file = project.getFile(path);
+    assertTrue(file.isSynchronized(IResource.DEPTH_ONE));
+
+    Properties properties = loadProperties(file);
+    assertEquals("io.takari.m2e.incrementalbuild.test", properties.getProperty("groupId"));
+    assertEquals("pomproperties", properties.getProperty("artifactId"));
+    assertEquals("0.1", properties.getProperty("version"));
+
+    // incremental no-change rebuild
+    recorder.clear();
+    project.build(IncrementalProjectBuilder.INCREMENTAL_BUILD, monitor);
+    waitForJobsToComplete();
+    assertNoErrors(project);
+    recorder.assertPaths(new String[0]);
+
+    //
+    recorder.clear();
+    copyContent(project, "pom.xml-changed", "pom.xml");
+    project.build(IncrementalProjectBuilder.INCREMENTAL_BUILD, monitor);
+    waitForJobsToComplete();
+    String pathChanged =
+        "target/classes/META-INF/maven/io.takari.m2e.incrementalbuild.test-changed/pomproperties/pom.properties";
+    recorder.assertPaths(path, pathChanged);
+    
+    IFile fileChanged = project.getFile(pathChanged);
+    assertFalse(file.exists());
+    assertTrue(fileChanged.isSynchronized(IResource.DEPTH_ONE));
+
+    properties = loadProperties(fileChanged);
+    assertEquals("io.takari.m2e.incrementalbuild.test-changed", properties.getProperty("groupId"));
+    assertEquals("pomproperties", properties.getProperty("artifactId"));
+    assertEquals("0.1", properties.getProperty("version"));
+  }
+
+  private Properties loadProperties(IFile file) throws IOException, CoreException {
+    Properties properties = new Properties();
+    try (InputStream is = file.getContents()) {
+      properties.load(is);
+    }
+    return properties;
+  }
+}
