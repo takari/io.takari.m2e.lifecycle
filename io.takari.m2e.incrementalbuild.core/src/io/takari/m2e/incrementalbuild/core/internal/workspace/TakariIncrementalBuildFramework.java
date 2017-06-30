19ca24f64d6817cd064f80a37bcaa2f020e80e1a
Jaime <jaime.morales@salesforce.com>
2017-06-30 15:39:24 -0700

    using thread context preserver to fix multithreaded builders

--- io.takari.m2e.incrementalbuild.core/src/io/takari/m2e/incrementalbuild/core/internal/workspace/TakariIncrementalBuildFramework.java
+++ io.takari.m2e.incrementalbuild.core/src/io/takari/m2e/incrementalbuild/core/internal/workspace/TakariIncrementalBuildFramework.java
@@ -17,6 +17,8 @@ import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResourceDelta;
 import org.eclipse.m2e.core.internal.builder.IIncrementalBuildFramework;
 
+import io.takari.builder.enforcer.PolicyContextPreserver;
+
 @SuppressWarnings("restriction")
 public class TakariIncrementalBuildFramework implements IIncrementalBuildFramework {
 
@@ -25,6 +27,7 @@ public class TakariIncrementalBuildFramework implements IIncrementalBuildFramewo
       BuildResultCollector results) {
     final AbstractBuildWorkspace workspace = newWorkspace(project, kind, delta, results);
     ThreadLocalBuildWorkspace.setDelegate(workspace);
+    PolicyContextPreserver.registerDelegate(new WorkspaceContextAccessor());
     return workspace;
   }
 
